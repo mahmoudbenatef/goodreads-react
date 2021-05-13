d8df5e928ac3fd64b969bc781a02b8384aae2b8b
Aya <ayahamedabdulaziz@gmail.com>
2021-05-13 23:19:44 +0200

    feature(shelf-component):: Created a reusable shelf dropdown component

--- src/API/BookServices.js
+++ src/API/BookServices.js
@@ -3,4 +3,7 @@ export const BookService =  {
   rate(userId, bookId, rating) {
     return API.post(`/books/${bookId}/rate`, { user: userId, rating: rating });
   },
+  shelve(userId, bookId, shelf) {
+    return api.post(`/books/${bookId}/shelve`, { user: userId, shelf: shelf });
+  }
 };
