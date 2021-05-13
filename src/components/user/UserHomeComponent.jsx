b76a5f5db77d233457499b5c1796e81704df4480
Aya <ayahamedabdulaziz@gmail.com>
2021-05-13 23:19:44 +0200

    feature(shelf-component):: Created a reusable shelf dropdown component

--- src/components/user/UserHomeComponent.jsx
+++ src/components/user/UserHomeComponent.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import { authContext } from "../../contexts/authContext";
 import RateComponent from "../reusableComponents/RateComponent";
+import ShelfComponent from "../reusableComponents/ShelfComponent";
 
 export default function UserHomeComponent() {
   const authentication = useContext(authContext);
@@ -20,7 +21,8 @@ export default function UserHomeComponent() {
   return (
     <>
       <h1>Hello User</h1>
-      <RateComponent bookId="60990a758d143e7cc21a02ac" userRating="3" size="small"></RateComponent>
+      <RateComponent bookId="609964ba4d02fa267a53acf4" userRating={2} size="small"></RateComponent>
+      <ShelfComponent bookId="609964ba4d02fa267a53acf4" ></ShelfComponent>
     </>
   );
 }
