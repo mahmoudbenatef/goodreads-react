fca63c05ff0ae25bff73ad3f8700a726d590c9de
Abobakr <abobakryousre@gmail.com>
2021-05-15 03:12:01 +0200

    feat(admin-books) rebase to dev

--- src/API/urls.js
+++ src/API/urls.js
@@ -2,22 +2,16 @@ import { mySessionStorage } from "../helper/LocalStorge";
 
 export const BASE_URL = "http://localhost:3001";
 export const API = {
-    register: () => `${BASE_URL}/users/register`,
-    signin: () => `${BASE_URL}/users/signin`,
-    getUsers: () => `${BASE_URL}/users/`,
-    category:()=> `${BASE_URL}/category/`,
-    token:()=> mySessionStorage.getToken(),
-};
-
-export const BOOK_API = {
-    rate: (bookId) => `${BASE_URL}/books/${bookId}/rate`,
-    shelve: (bookId) => `${BASE_URL}/books/${bookId}/shelve`
-};
   register: () => `${BASE_URL}/users/register`,
   signin: () => `${BASE_URL}/users/signin`,
   getUsers: () => `${BASE_URL}/users/`,
   category: () => `${BASE_URL}/category/`,
   token: () => mySessionStorage.getToken(),
+};
+
+export const BOOK_API = {
+  rate: (bookId) => `${BASE_URL}/books/${bookId}/rate`,
+  shelve: (bookId) => `${BASE_URL}/books/${bookId}/shelve`,
 };
 
 const booksURL = {
