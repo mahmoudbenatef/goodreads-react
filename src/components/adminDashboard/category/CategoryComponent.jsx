4054f6b567cc76152be0c7adcd5e3e423bb8fcf4
Atef <mahmoudbenatef@gmail.com>
2021-05-17 22:22:52 +0200

    add loading to category component

--- src/components/adminDashboard/category/CategoryComponent.jsx
+++ src/components/adminDashboard/category/CategoryComponent.jsx
@@ -7,6 +7,7 @@ export default function () {
     const [editOrCreate, setEditOrCreate] = useState({status: 'create'})
     const [category, setCatgory] = useState({status: 'add', value: {label: ''}})
     return (
+
         <div className={"d-flex flex-column min-vh-100 align-items-center  "}>
             <div className="row justify-content-center w-100 p-3 bg-light rounded-3">
                 <div className="row justify-content-center mt-5 flex-xl-shrink-2">
