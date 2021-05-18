c4a70233c79d53839be706d23aba93ba1e767284
Aya <ayahamedabdulaziz@gmail.com>
2021-05-18 16:40:54 +0200

    hotfix(admin): added keys to listed items and mofified styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import {
   BrowserRouter as Router,
-
   Redirect,
   Route,
   Switch
@@ -17,34 +16,21 @@ import NavbarComponent from "./components/user/NavbarComponent";
 import UserHomeComponent from "./components/user/UserHomeComponent";
 import { authContext } from "./contexts/authContext";
 
-
-
-
 function App(props) {
   const authentication = useContext(authContext);
   console.log(authentication);
   return (
     <>
       <Router>
-        <div className="d-flex flex-column min-vh-100" style={{backgroundColor:"#e3f2fd"}}>
-          {authentication.auth.authed === true &&
-            authentication.auth.role === "admin" && (
-              <>
-                <NavbarComponent parent="admin"></NavbarComponent>
-              </>
-            )}
-
-          {authentication.auth.authed === true &&
-            authentication.auth.role === "user" && (
-              <NavbarComponent></NavbarComponent>
-            )}
+        <div
+          className="d-flex flex-column min-vh-100"
+          style={{ backgroundColor: "#e3f2fd" }}
+        >
+          {authentication.auth.authed ===
+            true && (<NavbarComponent></NavbarComponent>)}
           {authentication.auth.authed === false &&
             BASE_URL + "/register" === window.location.pathname && (
-              <Redirect
-                to={{
-                  pathname: "/login",
-                }}
-              />
+              <Redirect to={{ pathname: "/login" }} />
             )}
           <Switch>
             <Route path="/admin">
@@ -62,7 +48,7 @@ function App(props) {
             <Route path="/register">
               <RegisterComponent />
             </Route>
-        
+
             <Route path="/authors">
               <AuthorsList />
             </Route>
