128929f03d19bdea17815c91cbba3c01204191c2
Aya <ayahamedabdulaziz@gmail.com>
2021-05-23 00:14:24 +0200

    hotfix(categories): Fix categories styling

--- src/components/user/category/ListCategoriesComponent.jsx
+++ src/components/user/category/ListCategoriesComponent.jsx
@@ -1,10 +1,11 @@
+import Button from "@material-ui/core/Button";
+import { makeStyles } from "@material-ui/core/styles";
 import { useContext, useEffect, useState } from "react";
-import {Route, Switch, useHistory, useRouteMatch, Link} from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import { userCategoriesServices } from "../../../API/userCategoriesServices";
 import { authContext } from "../../../contexts/authContext";
-
 export default function ListCategoriesComponent() {
-    const [userCategories, setUserCategories] = useState([]);
+  const [userCategories, setUserCategories] = useState([]);
   const authentication = useContext(authContext);
   const history = useHistory();
   useEffect(() => {
@@ -19,37 +20,72 @@ export default function ListCategoriesComponent() {
     }
   }, []);
 
+  useEffect(() => {
+    const getCategories = async () => {
+      const userCategoriesData = await userCategoriesServices.userCategories();
+      if (userCategoriesData.status === 200)
+        setUserCategories(userCategoriesData.data);
+      console.log(userCategoriesData.data);
+    };
+    getCategories();
+  }, []);
 
-    useEffect(()=>{
-        const getCategories = async () => {
-            const userCategoriesData = await userCategoriesServices.userCategories();
-            if (userCategoriesData.status === 200) setUserCategories(userCategoriesData.data);
-            console.log(userCategoriesData.data);
-        };
-        getCategories()
-      },[]);
-
+  const useStyles = makeStyles((theme) => ({
+    flex: {
+      display: "flex",
+      flexWrap: "wrap",
+      justifyContent: "space-around",
+      alignItems: "baseline",
+      alignContent: "space-between",
+      marginTop: "4rem",
+    },
+    category: {
+      width: "10rem",
+      height: "7rem",
+      textAlign: "center",
+      marginBottom: "2rem",
+      lineHeight: "7rem",
+    },
+    label: {
+      textDecoration: "none",
+      color: "black",
+      fontSize: "1.2rem",
+      "&:hover": {
+        color: "#ac5d59",
+      },
+    },
+  }));
+  const classes = useStyles();
   return (
     <>
-    {
-        userCategories.length === 0 
-        ? 
+      {userCategories.length === 0 ? (
         <h1>No Categories Available</h1>
-        :
-        <ul className="list-group list-group-horizontal">
-            { userCategories.map((category) => {
-                return <li className="list-group-item" key={category._id} > <Link to={{
-                    pathname: `/categories/${category._id}`,
-                    state: {
+      ) : (
+        <div className={classes.flex}>
+          {userCategories.map((category) => {
+            return (
+              <div className={classes.category} key={category._id}>
+                <Button
+                  style={{ padding: "1rem", width: "13rem" }}
+                  variant="outlined"
+                >
+                  <Link
+                  className={classes.label}
+                    to={{
+                      pathname: `/categories/${category._id}`,
+                      state: {
                         title: category.label,
-                    },
-                  }}>{ category.label }</Link></li>
-            })}
-        </ul>
-
-    }
-    
-
+                      },
+                    }}
+                  >
+                    {category.label}
+                  </Link>
+                </Button>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </>
   );
 }
